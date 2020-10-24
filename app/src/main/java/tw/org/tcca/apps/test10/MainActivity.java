63eb8be4529b1dbc4c045b9c30795f6f04c901f9
bradchao <brad@brad.tw>
2020-10-24 14:39:11 +0800

    5

--- app/src/main/java/tw/org/tcca/apps/test10/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test10/MainActivity.java
@@ -1,7 +1,11 @@
 package tw.org.tcca.apps.test10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,21 +17,46 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private TextView name;
+    private boolean isCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 123);
+        } else {
+            isCamera = true;
+            init();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            isCamera = true;
+        }else{
+            isCamera = false;
+        }
+        init();
+    }
+
+    private void init(){
         webView = findViewById(R.id.webview);
         name = findViewById(R.id.name);
         initWebView();
-
     }
 
     public class MyBrad {
         @JavascriptInterface
         public void startScan(){
+            if (isCamera){
+                
+            }
         }
     }
 
---
+++ app/src/main/java/tw/org/tcca/apps/test10/ScanActivity.java
@@ -0,0 +1,14 @@
+package tw.org.tcca.apps.test10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
